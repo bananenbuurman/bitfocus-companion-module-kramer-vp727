c58cf4621c9386a5439bb16bea9e225fa30b17f3
bananenbuurman <89082398+bananenbuurman@users.noreply.github.com>
2022-04-11 01:29:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -232,21 +232,6 @@ instance.prototype.actions = function(system) {
 							 { id: '8', label: 'Video'},
 							 { id: '9', label: 'HDMI'}											 
 						 ]
-				},
-				{
-					type: 'dropdown',
-					 label: 'Input',
-						 id: 'pgmId',
-						 choices: [
-							 { id: '0', label: '1'},
-							 { id: '1', label: '2'},
-							 { id: '2', label: '3'},
-							 { id: '3', label: '4'},
-							 { id: '4', label: '5'},
-							 { id: '5', label: '6'},
-							 { id: '6', label: '7'},
-							 { id: '7', label: '8'}
-						 ]
 				}
 
 			]
@@ -330,24 +315,13 @@ instance.prototype.actions = function(system) {
 				break;
 
 			case 'VideoType':                
-                		cmd = 'Y 0 95 '+ opt.vidtype;
-				cmd_pre = 'Y 0 94 '+ opt.pgmId;
+                		cmd = 'Y 0 95 '+ opt.vidtype;				
 				break; 				
 			case 'command':
 				cmd = opt.comId;
 				break;
 
 	};
-
-
-	if (cmd_pre !== undefined) {	
-		debug('sending cmd_pre ',cmd_pre,"to",self.config.host);
-		if (self.socket !== undefined && self.socket.connected) {
-			self.socket.send(cmd_pre + '\n');
-		} else {
-			debug('Socket not connected :(');
-		}
-	}
 
 
 	if (cmd !== undefined) {
